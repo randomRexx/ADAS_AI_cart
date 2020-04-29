74c858432a5c3103486112429f7557a3036ebe22
Jettypanini <57526189+Jettypanini@users.noreply.github.com>
2020-04-29 09:00:56 +0200

    Add files via upload
    IWR1642 with ROS

---
+++ src/ti_mmwave_rospkg/src/Radar_Read.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import rospy
+from ti_mmwave_rospkg.msg import RadarScan
+
+def callback(data):
+    print data
+
+def listener():
+    rospy.init_node('listener')
+    rospy.Subscriber("RadarScan", RadarScan, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
